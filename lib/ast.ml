18bd5956c02b2b9350a5ec523d7209b3798209b7
Lucas Ouellette-Falkenstein <fake@email.com>
2023-03-05 15:17:13 -0800

    Add command_to_string

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,9 +1,22 @@
-type command = { 
-  executable : string; 
-  args: string list;  
+type command = {
+  executable : string;
+  args : string list;
   (* Mapping of file descriptor to file name *)
-  redirections : (int * string) list
+  redirections : (int * string) list;
 }
 
 type pipeline = command list
 
+let command_to_string { executable; args; redirections } =
+  let redirection_to_string (fd, filename) =
+    string_of_int fd ^ "->" ^ filename
+  in
+  let bare_string =
+    [
+      executable;
+      String.concat " " args;
+      List.map redirection_to_string redirections |> String.concat ",";
+    ]
+    |> String.concat ";"
+  in
+  "{" ^ bare_string ^ "}"
