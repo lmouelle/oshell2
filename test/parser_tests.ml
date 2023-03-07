ea77db4b330a94ca13c0be45fe6edb916c954489
Lucas Ouellette-Falkenstein <fake@email.com>
2023-03-07 00:28:30 -0800

    Add a few more tests

--- test/parser_tests.ml
+++ test/parser_tests.ml
@@ -13,7 +13,29 @@ let test_multi_redirect _ =
       redirections = [ (2, "errfile"); (1, "outfile") ];
     }
 
+let test_simple_command _ =
+  let command = parse_string "ls -l -a -Z" in
+  assert_command_equals ~msg:"Test simple command" command
+    { executable = "ls"; args = [ "-l"; "-a"; "-Z" ]; redirections = [] }
+
+let test_redir_in_and_out _ =
+  let command =
+    parse_string "foo -Z < infile 2> errfile > outfile 666> etcfile"
+  in
+  assert_command_equals ~msg:"Test redir in and out" command
+    {
+      executable = "foo";
+      args = [ "-Z" ];
+      redirections =
+        [ (0, "infile"); (2, "errfile"); (1, "outfile"); (666, "etcfile") ];
+    }
+
 let suite =
-  "Parser tests" >::: [ "Test multi redirect" >:: test_multi_redirect ]
+  "Parser tests"
+  >::: [
+         "Test multi redirect" >:: test_multi_redirect;
+         "Test simple command" >:: test_simple_command;
+         "Test redire in and out" >:: test_redir_in_and_out;
+       ]
 
 let _ = run_test_tt_main suite
