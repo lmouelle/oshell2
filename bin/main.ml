8def46d98f1c0f516dd78582551686a9730670b0
Lucas Ouellette-Falkenstein <fake@email.com>
2023-03-07 00:45:12 -0800

    Impl main loop

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,26 @@
-let () = print_endline "Hello, World!"
+open Oshell2.Parseutils
+open Oshell2.Executor
+
+let lastexitcode = ref (-1)
+
+let exec str =
+  let command = parse_string str in
+  exec_command command
+
+let repl _ =
+  print_newline ();
+  let rec repl' _ =
+    print_string "> ";
+    let ln = read_line () in
+    lastexitcode := exec ln;
+    repl' ()
+  in
+  try repl' () with End_of_file -> ()
+
+let () =
+  if Array.length Sys.argv > 2 then (
+    Printf.printf "Usage: oshell [script]";
+    exit 64)
+  else if Array.length Sys.argv == 2 then
+    failwith "TODO: Impl running files"
+  else repl ()
