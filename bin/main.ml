b8ca6d1b76671a3471db3e265aa18a1abef5e755
Lucas Ouellette-Falkenstein <fake@email.com>
2023-03-07 23:22:12 -0800

    Remove the $? impl from main repl for now

--- bin/main.ml
+++ bin/main.ml
@@ -6,12 +6,8 @@ let repl _ =
   let rec repl' _ =
     print_string "> ";
     let ln = read_line () in
-    (* TODO: Implement variables properly and just make last exit code a case of variables *)
-    if (String.trim ln = "$?") 
-    then Printf.printf "%d\n" (get_last_exit_code ()) 
-    else 
-      let prog = parse_string ln in
-      exec_pipeline prog;
+    let prog = parse_string ln in
+    exec_pipeline prog;
     repl' ()
   in
   try repl' () with End_of_file -> ()
