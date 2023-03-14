8ce305b10c255fb09452e46b737f9fe03a915352
Lucas Ouellette-Falkenstein <fake@email.com>
2023-03-14 01:56:16 -0700

    Catch unknown var exceptions

--- bin/main.ml
+++ bin/main.ml
@@ -4,13 +4,17 @@ open Oshell2.Executor
 let repl _ =
   print_newline ();
   let rec repl' prev_shell_vars =
-    print_string "> ";
-    let ln = read_line () in
-    let prog = parse_string ln in
-    let {shell_vars; exitcode = _} = exec prev_shell_vars prog in
-    repl' (shell_vars @ prev_shell_vars)
+    try
+      print_string "> ";
+      let ln = read_line () in
+      let prog = parse_string ln in
+      let {shell_vars; exitcode = _} = exec prev_shell_vars prog in
+      repl' (shell_vars @ prev_shell_vars)
+    with
+    | End_of_file -> ()
+    | NoSuchVar name -> (Printf.printf "No known var named: %s"  name)
   in
-  try repl' [] with End_of_file -> ()
+  repl' []
 
 let () =
   if Array.length Sys.argv > 2 then (
