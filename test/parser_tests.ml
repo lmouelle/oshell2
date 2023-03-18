ce070e6dac9cda9bc6b510b413ab24be67171001
Lucas Ouellette-Falkenstein <fake@email.com>
2023-03-17 19:41:21 -0700

    Replace ounit tests with cram tests

--- test/parser_tests.ml
+++
@@ -1,2 +0,0 @@
-(* TODO: Rewrite all these tests once I have implemented modules for abstraction so
-   I do not have to change everything everywhere everytime I want to update my grammar *)
---
+++ test/test1.t
@@ -0,0 +1,4 @@
+Test basic output works
+  $ oshell2 -c "printf 5\n"
+  5
+
